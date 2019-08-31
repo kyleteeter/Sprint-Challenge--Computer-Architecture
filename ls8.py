7f91fbd2fc5c4c505b582003f8d635095e035883
unknown <kyleteeter12@gmail.com>
2019-08-31 11:36:49 -0500

    Modified ls8.py

--- ls8.py
+++ ls8.py
@@ -5,10 +5,7 @@
 import sys
 from cpu import *
 
-if len(sys.argv) != 2:
-  print('Please provide a filename to execute instructions.')
-  sys.exit(1)
-else:
-  cpu = CPU() 
-  cpu.load(sys.argv[1])
-  cpu.run()
+
+cpu = CPU() 
+cpu.load(sys.argv[1])
+cpu.run()
